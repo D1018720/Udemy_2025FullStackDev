acac44b36c884095629c1da4898cc9d68075702b
D1018720 <D1018720@o365.fcu.edu.tw>
2025-07-03 10:18:06 +0000

    Lexical Structure

---
+++ chapter5/ch5-2/app.js
@@ -0,0 +1,17 @@
+// 常見的JavaScript函數
+// console.log() - 將message輸出到Web控制台。
+// window.alert() - 指示瀏覽器顯示帶有可選消息的對話框，並等待用戶關閉對話框。
+// window.prompt() - 指示瀏覽器顯示帶有可選消息的對話框，並等待用戶輸入文本，或取消對話框。
+
+// JS中函數可以連續使用，例如以下:
+// let user_name = window.prompt("請輸入你的姓名");
+// window.alert("！歡迎來到我們的網站！" + user_name);
+
+// Lexical Structure是一組基本規則，用於指定如何用該語言編寫程式。
+// Case Sensitve - JS中大小寫有差。
+// 空白鍵、換行鍵等在JS當中會被全部忽略。通常放到伺服器上的JS程式碼都會被做minification(刪除空白鍵、換行鍵)。
+// JS中註解是//，多行則是被/**/包住。
+// JS內的變數名稱須由文字、underscore(_)、dollar sign($)當開頭，不能用數字當開頭。
+// JS內有關鍵字不能當變數名稱。
+// JS使用Unicode字元集合，所以String內可由任何Unicode文字組成。
+// ;可用來分隔JS的程式碼。
