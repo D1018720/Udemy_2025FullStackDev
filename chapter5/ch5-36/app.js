6a0b666cf4661e9f571897183a02570926063a20
D1018720 <D1018720@o365.fcu.edu.tw>
2025-07-06 11:30:11 +0000

    反轉矩陣

---
+++ chapter5/ch5-36/app.js
@@ -0,0 +1,5 @@
+const friends = ["Alice", "Bob", "Charlie", "David", "Eve"];
+let reverseFriends = [];
+
+reverseFriends = friends.reverse();
+console.log(reverseFriends); // ["Eve", "David", "Charlie", "Bob", "Alice"] 
