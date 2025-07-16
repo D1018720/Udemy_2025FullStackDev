f19f0fb04535c4b7d55b44191e511d0f74da7934
D1018720 <D1018720@o365.fcu.edu.tw>
2025-07-16 13:49:19 +0000

    JS語言類型

---
+++ chapters/chapter10/ch10-2/app.js
@@ -0,0 +1,9 @@
+// Strong typing and Weak typing
+// Strong typing意味著值得資料類型在有需要時，是必須要被強制改成正確的類別。
+// JS是弱類型語言，這意味著你可以將一個值賦給一個變數，而不需要指定它的類型。
+
+// Dynamic typing and Static typing
+// Static typed語言通常指編譯器會在編譯時檢查資料類型，dynamic typed語言則是在執行時檢查資料類型。
+// Static typed語言通常執行更快，因為當編譯器知道正在使用的確切數據類型時，
+// 他可以生成優化的機器代碼來運行程式，且使用更少的記憶體。
+// JS是dynamic typed
