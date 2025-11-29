5ccdf96a7645e2a84233a58a0606146fc8500dc6
D1018720 <D1018720@o365.fcu.edu.tw>
2025-11-29 14:15:46 +0800

    Delete chapters/chapter12 directory

--- chapters/chapter12/ch12-4/myModule/app1.js
+++
@@ -1,3 +0,0 @@
-let app2 = require('./app2.js');
-console.log(app2.morning());
-console.log(app2.evening());
--- chapters/chapter12/ch12-4/myModule/index.js
+++
@@ -1,5 +0,0 @@
-let app1 = require('./app1.js');
-let app2 = require('./app2.js');
-
-exports.morning = app2.morning;
-exports.evening = app2.evening;
