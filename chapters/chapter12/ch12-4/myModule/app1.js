7c127fa3ed0cf5d258fdc1100b82fccc373a786d
D1018720 <D1018720@o365.fcu.edu.tw>
2025-11-29 06:48:39 +0000

    ch12-4 self-made modules, ch12-5  node內建module, ch12-8 網頁伺服器製作, ch12-11 NPM與nodemon

---
+++ chapters/chapter12/ch12-4/myModule/app1.js
@@ -0,0 +1,3 @@
+let app2 = require('./app2.js');
+console.log(app2.morning());
+console.log(app2.evening());
---
+++ chapters/chapter12/ch12-4/myModule/index.js
@@ -0,0 +1,5 @@
+let app1 = require('./app1.js');
+let app2 = require('./app2.js');
+
+exports.morning = app2.morning;
+exports.evening = app2.evening;
