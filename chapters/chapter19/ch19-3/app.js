f40b4514fd4e1f34453f67dff9fd3deff24381eb
D1018720 <D1018720@o365.fcu.edu.tw>
2025-12-06 12:45:36 +0000

    ch19-12 透過網頁更新學生資料

--- chapters/chapter19/ch19-3/app.js
+++ chapters/chapter19/ch19-3/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Student = require('./models/student');
 // 引入 cors middleware 解決資安跨域問題
 // const cors = require('cors');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://root:root@mongo:27017/app?authSource=admin')
   .then(() => {
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // 引入 cors middleware 解決資安跨域問題
 // app.use(cors());
+app.use(methodOverride('_method'));
 
 // 獲得所有學生的資料
 app.get('/students', async (req, res) => {
@@ -65,6 +67,22 @@ app.get('/students/:_id', async (req, res) => {
     }
 });
 
+// 透過網頁修改學生資料的表單頁面
+app.get('/students/:_id/edit', async (req, res) => {
+    let {_id} = req.params;
+    try {
+        let foundStudent = await Student.findOne({ _id }).exec()
+        if (foundStudent != null) {
+            return res.render('edit-student', { foundStudent });
+        } else {
+            return res.status(400).render('student-not-found');
+        }
+    } catch (e) {
+        console.log(e);
+        return res.status(400).render('student-not-found');
+    }
+});
+
 app.post('/students', async (req, res) => {
     try {
         let { name, age, major, merit, other } = req.body;
@@ -102,7 +120,7 @@ app.put('/students/:_id', async (req, res) => {
             // overwrite: true 的意思就是用新的資料覆蓋掉舊的資料
             },
         );
-        return res.send({ msg: "學生資料更新成功", updateData: newData });
+        return res.render('student-update-success', { newData });
     } catch (e) {
         return res.status(400).send(e.message);
     }
