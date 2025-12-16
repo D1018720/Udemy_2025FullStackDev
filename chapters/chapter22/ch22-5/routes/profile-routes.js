c28057c3d0834e819d007c05615e7ce3bd5b0979
D1018720 <D1018720@o365.fcu.edu.tw>
2025-12-16 14:12:12 +0000

    ch22-11 製作Post

--- chapters/chapter22/ch22-5/routes/profile-routes.js
+++ chapters/chapter22/ch22-5/routes/profile-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const Post = require('../models/post-model');
 
 const authCheck = (req, res, next) => {
     if (req.isAuthenticated()) {
@@ -10,8 +11,30 @@ const authCheck = (req, res, next) => {
     }
 };
 
-router.get('/', authCheck, (req, res) => {
-    return res.render('profile', { user: req.user }); //deSerializeUser會把使用者資料放在req.user
+router.get('/', authCheck, async (req, res) => {
+    let postFound = await Post.find({ author: req.user._id }).exec();
+    return res.render('profile', { user: req.user, posts: postFound }); //deSerializeUser會把使用者資料放在req.user
+});
+
+router.get('/post', authCheck, (req, res) => {
+    return res.render('post', { user: req.user });
+});
+
+router.post('/post', authCheck, async (req, res) => {
+    let { title, content } = req.body;
+    let newPost = new Post({
+        title,
+        content,
+        author: req.user._id,
+    });
+    try {
+        await newPost.save();
+        return res.redirect('/profile');
+    } catch (e) {
+        req.flash("error_msg", "標題與內容都需要填寫。");
+        return res.redirect('/profile/post');
+    }
+    
 });
 
 module.exports = router;
